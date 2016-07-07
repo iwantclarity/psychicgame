4618691133c44726f63f30c8de53c2f1d0f26168
David Forman <djforman@gmail.com>
2016-07-07 17:57:11 -0400

    Finished Project

--- assets/game.js
+++ assets/game.js
@@ -3,12 +3,13 @@
 // Sets the computer choices 
 var computerChoices = ['a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var historychoices =[];
+var letDowns = ['Arent you sweet! That letter is not it!', 'Youre in denial cause that letter is not right, man', 'A Libra would of guessed that right', 'Alright baby cakes, that was wrong', 'Is there a new gentleman in your life, because he might of had the right answer.'];
 
 // Declares the tallies to 0 
 var wins = 0;
 var losses = 0;
 var guesses = 9;
-var m = "How are you today?";
+var m = "Lets Begin...";
 
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
@@ -24,7 +25,7 @@ document.onkeyup = function(event) {
 
 
 	if (historychoices.includes(userGuess) ) {
-			alert ("While things don't always make sense, things happen for a reason. You picked the same letter again not even Miss Cleo knows why!");
+			var m = "While things don't always make sense, things happen for a reason. You picked the same letter again not even Miss Cleo knows why!";
 	}
 
 	// Making sure the user has guesses available
@@ -37,11 +38,14 @@ document.onkeyup = function(event) {
 			guesses = 9;
 			computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 			historychoices = [];
+			var m = "Miss Cleo aleady saw you do it and knew you were going to guess it right.";
 
 		}else if ((userGuess != computerGuess)){
 			guesses--;
 			historychoices.push(userGuess);
-			me = "Arent you sweet!";
+
+
+			m = letDowns[Math.floor(Math.random() * letDowns.length)];
 
 		}
 
@@ -55,13 +59,14 @@ document.onkeyup = function(event) {
 		losses++;
 		alert ("You Lose");
 		historychoices = [];
+		m = "Lets Begin..."
 	}
 
 		var html = "<h1>Pick a letter and see if you can guess which letter Miss Cleo is thinking!</h1>" +
-		"<h2>wins: " + 
+		"<h2>Wins: " + 
 		wins + 
 		"</h2>" +
-		"<h2>losses: " + 
+		"<h2>Losses: " + 
 		losses + 
 		"</h2>" +
 		"<h2>Guesses Left: " + 
@@ -69,7 +74,7 @@ document.onkeyup = function(event) {
 		"</h2>" +
 		"<h2>Letters Used: " + 
 		historychoices + 
-		"</h2>"
+		"</h2>" +
 		"<h2>" + 
 		m + 
 		"</h2>";
